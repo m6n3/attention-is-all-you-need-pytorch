35107c5f43546d9aaad5244930e5146683cf178e
m6n3 <benji.mousavi@gmail.com>
2023-08-16 04:33:59 +0000

    add positional embedding module

---
+++ transformer/positional_embedding.py
@@ -0,0 +1,40 @@
+import math
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, embed_dim, vocab_size, max_seq_len, dropout) -> None:
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.pe = self.build_pe_matrix(max_seq_len, embed_dim)
+        self.scale = math.sqrt(embed_dim)
+
+    def build_pe_matrix(self, max_seq_len, embed_dim):
+        pe = torch.zeros(max_seq_len, embed_dim)
+        for pos in range(max_seq_len):
+            for i in range(0, embed_dim, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** (i / embed_dim)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** (i / embed_dim)))
+        # pe: [max seq len, embed dim]
+
+        return pe
+
+    def forward(self, src):
+        # src: [batch size, seq len]
+
+        embed = self.embedding(src)
+        # embed: [batch size, seq len, embed dim]
+
+        seq_len = src.size(1)
+        positional_encoding = Variable(self.pe[:seq_len, :], requires_grad=False)
+        # positional_encoding: [seq len, embed size]
+
+        positional_embedding = (
+            embed * self.scale + positional_encoding
+        )  # `+` takes care of size mismatch of the operands.
+        # positional_embedding: [batch size, seq len, embed dim]
+
+        return self.dropout(positional_embedding)
---
+++ transformer/positional_embedding_test.py
@@ -0,0 +1,21 @@
+import positional_embedding as pe
+
+import torch
+import unittest
+
+
+class TestPositionalEmbedding(unittest.TestCase):
+    def test_basics(self):
+        embed_dim, vocab_size, max_seq_len, dropout = 512, 1_000_000, 100, 0.1
+
+        embedder = pe.PositionalEmbedding(embed_dim, vocab_size, max_seq_len, dropout)
+
+        batch_size, seq_len = 5, 50
+        x = torch.randint(vocab_size, (batch_size, seq_len))
+        out = embedder(x)
+
+        self.assertTrue(out.shape == torch.Size([batch_size, seq_len, embed_dim]))
+
+
+if __name__ == "__main__":
+    unittest.main()
