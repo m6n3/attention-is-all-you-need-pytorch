555d71b8be403e81d2cbd4986a1cae9746894a74
m6n3 <benji.mousavi@gmail.com>
2023-08-18 06:51:54 +0000

    add encoder

---
+++ transformer/encoder.py
@@ -0,0 +1,48 @@
+from encoder_layer import EncoderLayer
+from positional_embedding import PositionalEmbedding
+
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        vocab_size,
+        embed_dim,
+        feedforward_dim,
+        num_layers,
+        num_heads,
+        max_seq_len,
+        dropout,
+    ):
+        super().__init__()
+        self.positional_embedding = PositionalEmbedding(
+            embed_dim=embed_dim,
+            vocab_size=vocab_size,
+            max_seq_len=max_seq_len,
+            dropout=dropout,
+        )
+        self.encoder_layers = nn.ModuleList(
+            [
+                EncoderLayer(
+                    embed_dim=embed_dim,
+                    feedforward_dim=feedforward_dim,
+                    num_heads=num_heads,
+                    dropout=dropout,
+                )
+                for _ in range(num_layers)
+            ]
+        )
+
+    def forward(self, x, mask):
+        # x: [batch size, seq len]
+
+        x = self.positional_embedding(x)
+        # x: [batch size, seq len, embed dim]
+
+        for layer in self.encoder_layers:
+            x = layer(x, mask)
+        # x: [batch size, seq len, embed dim]
+
+        return x
---
+++ transformer/encoder_test.py
@@ -0,0 +1,29 @@
+from encoder import Encoder
+
+import torch
+import unittest
+
+
+class TestEncoder(unittest.TestCase):
+    def test_basics(self):
+        with torch.no_grad():
+            vocab_size, embed_dim = 1000_000, 512
+            e = Encoder(
+                vocab_size=vocab_size,
+                embed_dim=512,
+                feedforward_dim=2048,
+                num_layers=1,
+                num_heads=8,
+                max_seq_len=100,
+                dropout=0.1,
+            )
+            batch_size, seq_len = 50, 100
+            x = torch.randint(vocab_size, (batch_size, seq_len))
+            mask = torch.rand(batch_size, 1, 1, seq_len) > 0.5
+
+            y = e(x, mask)
+            self.assertEqual(y.shape, torch.Size([batch_size, seq_len, embed_dim]))
+
+
+if __name__ == "__main__":
+    unittest.main()
